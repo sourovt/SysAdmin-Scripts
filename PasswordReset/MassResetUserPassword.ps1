1a8fa69d58d9286c290e17b46e41f787713dc3b1
Sourov Talukder <sourov.talukder@yahoo.com>
2026-09-14 17:20:12 -0400

    Add files via upload

---
+++ PasswordReset/MassResetUserPassword.ps1
@@ -0,0 +1,8 @@
+﻿# The script below takes all the users from a group and resets their password to specified password.
+
+$accounts = Get-ADGroupMember -Identity "GroupName" | Select-Object Name | Sort-Object Name
+
+foreach($account IN $accounts)
+    {
+        Set-ADAccountPassword -Identity $account.Name -Reset -NewPassword (ConvertTo-SecureString -AsPlainText "NewPassword" -Force)
+    }
